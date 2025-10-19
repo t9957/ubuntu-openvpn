6add0c3125d08b9f54a91c8e8551875b42923698
t9957 <t995771take@gmail.com>
2025-10-19 16:59:54 +0900

    put the settings etc. into the volume and cut it

--- sh/preparation.sh
+++ sh/preparation.sh
@@ -4,15 +4,15 @@ echo "==> preparation.sh: 開始"
 
 CONF_DIR="/etc/openvpn"
 
-cd "$CONF_DIR"
+cd "$CONF_DIR/ca"
 
 
-if [ ! -d "$CONF_DIR/ca" ]; then
+if [ ! -d "$CONF_DIR/ca/easyrsa" ]; then
 
   echo "==> preparation.sh: dir:ca無し"
 
-  make-cadir ca
-  cd ca
+  make-cadir easyrsa
+  cd easyrsa
   
   echo 'set_var EASYRSA_REQ_COUNTRY    "JP"' >> vars
   echo 'set_var EASYRSA_REQ_PROVINCE   "Tokyo"' >> vars
@@ -40,8 +40,12 @@ if [ ! -d "$CONF_DIR/ca" ]; then
   openvpn --genkey secret ta.key
   echo "==> preparation.sh: genkey完了"
 
-  cp pki/ca.crt pki/issued/server.crt pki/private/server.key pki/dh.pem ta.key /
-  echo "==> preparation.sh: cp完了"
-  else
+  # pki/ca.crt
+  # pki/issued/server.crt
+  # pki/private/server.key
+  # pki/dh.pem
+  # ta.key
+else
   echo "==> preparation.sh: dir:ca有り"
+  # tail -f /dev/null
 fi
