980b597e5c64bacb9d7e1140dd5a46ffdfddb966
t9957 <t995771take@gmail.com>
2025-10-22 21:01:46 +0900

    code related to MTU

--- sh/up.sh
+++ sh/up.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-# up.sh : コンテナ内でNAT設定を行う
-
-# コンテナ内でのethデバイスを自動検出
 OUT_IF=$(ip route | awk '/default/ {print $5}')
+sysctl -w net.ipv4.ip_forward=1
 
-# IP転送有効化
-echo "==> up.sh: "$(sysctl -w net.ipv4.ip_forward=1)
-
-# NAT有効化
-echo "==> up.sh: "$(iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o $OUT_IF -j MASQUERADE)
+# NAT監視ループ
+(while true; do
+  if ! iptables -t nat -C POSTROUTING -s 10.8.0.0/24 -o "$OUT_IF" -j MASQUERADE 2>/dev/null; then
+    echo "==> up.sh: iptables無し 設定実行"
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o "$OUT_IF" -j MASQUERADE
+  fi
+  sleep 30
+done) &
